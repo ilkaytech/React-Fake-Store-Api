ab651c2218c9cf1c8a8dc74bd40b1c165a3b5765
ilkay <06ilkayozturk@gmail.com>
2024-03-07 21:31:32 +0300

    create home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,28 @@
+import { useContext } from "react";
+import { ProductContext } from "../contexts/ProductContext";
+import Product from "../components/Product";
+
 export default function Home() {
-  return <div>Home</div>;
+  // get products from product context
+  const products = useContext(ProductContext);
+  // get only men's & women's clothing category
+  const filteredProducts = products.filter((item) => {
+    return (
+      item.category === "men's clothing" || item.category === "women's clothing"
+    );
+  });
+
+  return (
+    <div>
+      <section className="py-16 px-36">
+        <div className="container mx-auto">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-7 max-w-sm mx-auto md:max-w-none md:mx-0 ">
+            {filteredProducts.map((product) => {
+              return <Product product={product} key={product.id} />;
+            })}
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,24 @@ export default {
     "./node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    container: {
+      padding: {
+        DEFAULT: "30px",
+        lg: "0",
+      },
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1440px",
+    },
+    extend: {
+      colors: {
+        primary: "#222222",
+        secondary: "#F5E6E0",
+      },
+    },
   },
   plugins: [require("flowbite/plugin")],
 };
